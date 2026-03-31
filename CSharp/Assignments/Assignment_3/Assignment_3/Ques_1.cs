d6660e94b82395bdf72e0b3de26044c5c7971674
Beloni Rakal <belonir@infinite.com>
2026-03-31 22:36:05 +0530

    Adding ques1

--- CSharp/Assignments/Assignment_3/Assignment_3/Ques_1.cs
+++ CSharp/Assignments/Assignment_3/Assignment_3/Ques_1.cs
@@ -1,15 +1,94 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assignment_3
 {
-    internal class Program
+    // Account Class
+    class Account
+    {
+        int acc_no;
+        string cust_name;
+        string acc_type;
+        double bal;
+
+        public Account(int acc_no, string cust_name, string acc_type, double bal)
+        {
+            this.acc_no = acc_no;
+            this.cust_name = cust_name;
+            this.acc_type = acc_type;
+            this.bal = bal;
+        }
+
+        public void Credit(double amount)
+        {
+            bal += amount;
+            Console.WriteLine("Amount Deposited");
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount <= bal)
+            {
+                bal -= amount;
+                Console.WriteLine("Amount Withdrawn");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+
+        public void Transaction(char type, double amount)
+        {
+            if (type == 'D' || type == 'd')
+                Credit(amount);
+            else if (type == 'W' || type == 'w')
+                Debit(amount);
+            else
+                Console.WriteLine("Invalid Transaction");
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("\n--- ACCOUNT DETAILS ---");
+            Console.WriteLine("Account Number : " + acc_no);
+            Console.WriteLine("Customer Name  : " + cust_name);
+            Console.WriteLine("Account Type   : " + acc_type);
+            Console.WriteLine("Balance        : " + bal);
+        }
+    }
+
+    // Main Class for Question 1
+    internal class Ques_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("---- ACCOUNT PROGRAM ----");
+
+            Console.Write("Enter Account Number: ");
+            int accNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Customer Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Account Type: ");
+            string type = Console.ReadLine();
+
+            Console.Write("Enter Initial Balance: ");
+            double balance = Convert.ToDouble(Console.ReadLine());
+
+            Account acc = new Account(accNo, name, type, balance);
+
+            Console.Write("\nEnter Transaction Type (D/W): ");
+            char transType = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter Amount: ");
+            double amt = Convert.ToDouble(Console.ReadLine());
+
+            acc.Transaction(transType, amt);
+
+            acc.ShowData();
+
+            Console.ReadLine();
         }
     }
 }
