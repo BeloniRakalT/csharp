6f8161ae9903bebf3ca6ebab5a238b7188d73117
Beloni Rakal <belonir@infinite.com>
2026-04-03 20:37:02 +0530

    Added Files in Assignment-4

---
+++ CSharp/Assignments/Assignment-4/Assignment-4/Ques-2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assignment_4
+{
+    internal class Ques_2
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            if (input.Length > 1)
+            {
+                char first = input[0];
+                char last = input[input.Length - 1];
+
+                string middle = input.Substring(1, input.Length - 2);
+
+                string result = last + middle + first;
+
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                // If string has only 1 character, return it as it is
+                Console.WriteLine("Result: " + input);
+            }
+        }
+    }
+}
---
+++ CSharp/Assignments/Assignment-4/Assignment-4/Ques-3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment_4
+{
+    internal class Ques_3
+    {
+        public static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            Console.Write("Enter number of elements: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter elements:");
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = Convert.ToInt32(Console.ReadLine());
+                stack.Push(value);
+            }
+
+            // Convert stack to list
+            List<int> list = new List<int>(stack);
+
+            // Sort in descending order
+            list.Sort();
+            list.Reverse();
+
+            // Create new stack with sorted values
+            Stack<int> sortedStack = new Stack<int>();
+
+            foreach (int item in list)
+            {
+                sortedStack.Push(item);
+            }
+
+            Console.WriteLine("Stack elements in descending order:");
+
+            foreach (int item in sortedStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
