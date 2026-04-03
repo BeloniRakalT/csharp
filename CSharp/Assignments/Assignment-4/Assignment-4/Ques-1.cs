c7f2753cf1dbc09dd60b553ddd754dc0d2e2816b
Beloni Rakal <belonir@infinite.com>
2026-04-03 20:31:07 +0530

    Added Assignment_4 folder with solutions

---
+++ CSharp/Assignments/Assignment-4/Assignment-4/Ques-1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            Console.Write("Enter position to remove: ");
+            int position = Convert.ToInt32(Console.ReadLine());
+
+            if (position >= 0 && position < input.Length)
+            {
+                string result = input.Remove(position, 1);
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid position!");
+            }
+        }
+    }
+}
