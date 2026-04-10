b721f9a38e24ea5a7b7032618e53297565ff498f
Beloni Rakal <belonir@infinite.com>
2026-04-10 12:02:21 +0530

    Added selected files

---
+++ CSharp/Assignments/Assignment-7/ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Concession
+    {
+        public string CalculateConcession(int age)
+        {
+            const int TotalFare = 500;
+
+            if (age <= 5)
+            {
+                return "Little Champs - Free Ticket";
+            }
+            else if (age > 60)
+            {
+                double fare = TotalFare - (TotalFare * 0.30);
+                return "Senior Citizen - " + fare;
+            }
+            else
+            {
+                return "Ticket Booked - " + TotalFare;
+            }
+        }
+    }
+}
